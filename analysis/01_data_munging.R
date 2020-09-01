e6982f1d8525b04a0eb7bc388d5e0e84f811b032
mdscheuerell <scheuerl@uw.edu>
2020-08-31 17:39:16 -0700

    added colname switches for site files

--- analysis/01_data_munging.R
+++ analysis/01_data_munging.R
@@ -28,7 +28,7 @@ colnames_mon <- c("Year", "Month", "WaterYear", "WaterYearMonth", "CSMeanTempC",
 
 ## col names for annual data
 colnames_ann <- c("WaterYear", "CSMeanTempC", "CSPrecipitationmm", "Runoffmm", 
-                  "StrCamgL", "StrDOCmgL", "StrNH4NmgL", "StrNO3NmgL", "StrTPmgL", 
+                  "StrCamgL", "StrDOCmgL", "StrNH4NmgL", "StrNO3NmgL", "StrTDPmgL", 
                   "StrSO4SmgL", "depNH4Nkgha", "depNO3Nkgha", "depTotNkgha", "depSO4Skgha", 
                   "NAMGridTempC", "NAMGridPrecipitationmm", "FWACamgL", "FWADOCmgL", 
                   "FWANH4NmgL", "FWANO3NmgL", "FWATPmgL", "FWASO4SmgL", "iNDVI")
@@ -56,9 +56,17 @@ for(dd in data_dirs) {
     ## read file
     tmp_m <- readr::read_csv(file.path(data_dir, mm),
                              na = c("", "NA", "N/A", "#DIV/0!", "#VALUE!"))
-    ## remove spaces from col names
-    colnames(tmp_m) <- sub(" ", "", colnames(tmp_m))
-    
+    ## remove spaces & commas from col names
+    colnames(tmp_m) <- gsub(" ", "", colnames(tmp_m))
+    colnames(tmp_m) <- gsub(",", "", colnames(tmp_m))
+    ## replace different names for P
+    colnames(tmp_m) <- gsub("TPmgL", "TDPmgL", colnames(tmp_m))
+    colnames(tmp_m) <- gsub("PO4PmgL", "TDPmgL", colnames(tmp_m))
+    ## replace different names for N
+    colnames(tmp_m) <- gsub("NO3NO2NmgL", "NO3NmgL", colnames(tmp_m))
+    ## replace different names for C
+    colnames(tmp_m) <- gsub("TOCTCICmgL", "DOCmgL", colnames(tmp_m))
+
     ## check if file col names match set names
     col_chk <- colnames_mon %in% colnames(tmp_m)
     if(!all(col_chk)) {
@@ -68,7 +76,7 @@ for(dd in data_dirs) {
       tmp_m <- cbind(tmp_m, mis)
     }
     
-    ## add watershed type and site name
+    ## add watershed type, site & catchment
     tmp_m <- cbind(type = dd,
                    site = sub("([A-Z]{3,4})(_.*)", "\\1", mm),
                    catchment = sub("([A-Z]{3,4}_)(.*)(_monthly.csv)", "\\2", mm),
@@ -92,8 +100,16 @@ for(dd in data_dirs) {
     ## read file
     tmp_a <- readr::read_csv(file.path(data_dir, aa),
                              na = c("", "NA", "N/A", "#DIV/0!", "#VALUE!"))
-    ## remove spaces from col names
-    colnames(tmp_a) <- sub(" ", "", colnames(tmp_a))
+    ## remove spaces & commas from col names
+    colnames(tmp_a) <- gsub(" ", "", colnames(tmp_a))
+    colnames(tmp_a) <- gsub(",", "", colnames(tmp_a))
+    ## replace different names for P
+    colnames(tmp_a) <- gsub("TPmgL", "TDPmgL", colnames(tmp_a))
+    colnames(tmp_a) <- gsub("PO4PmgL", "TDPmgL", colnames(tmp_a))
+    ## replace different names for N
+    colnames(tmp_a) <- gsub("NO3NO2NmgL", "NO3NmgL", colnames(tmp_a))
+    ## replace different names for C
+    colnames(tmp_a) <- gsub("TOCTCICmgL", "DOCmgL", colnames(tmp_a))
     
     ## check if file col names match set names
     col_chk <- colnames_ann %in% colnames(tmp_a)
@@ -104,7 +120,7 @@ for(dd in data_dirs) {
       tmp_a <- cbind(tmp_a, mis)
     }
     
-    ## add watershed type and site name
+    ## add watershed type, site & catchment
     tmp_a <- cbind(type = dd,
                    site = sub("([A-Z]{3,4})(_.*)", "\\1", aa),
                    catchment = sub("([A-Z]{3,4}_)(.*)(_wateryearannual.csv)", "\\2", aa),
