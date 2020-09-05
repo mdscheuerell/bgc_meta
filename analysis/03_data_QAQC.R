7c22fffefb3fc6bf482065a549f1ec142eaa8220
Tamara <tkharms@alaska.edu>
2020-09-04 20:53:12 -0800

    monthly flow-weighted plots for QAQC
    Ca: check Sleepers high outliers: why does LEF have monthly FWACa but not annual?; LEF units are incorrect
    DOC: LEF DOC units are incorrect, check outliers for HBEF
    NH4: LEF incorrect units
    NO3: LEF incorrect units
    TDP: erroneous values at beginning of CWT record?
    SO4: are the outliers at SLP and TLW real?

--- analysis/03_data_QAQC.R
+++ analysis/03_data_QAQC.R
@@ -25,8 +25,6 @@ Ca.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrCamgL, color = catchment, 
 
 # 3 points in error at SLP (should be <80 mg/L at Sleepers)? Same 3 years have elevated DOC
 
-ggsave(path = "plots", filename = "Ca.an.pdf", width = 11, height = 8)
-
 # DOC
 DOC.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrDOCmgL, color = catchment, shape = type)) +
   geom_point() +
@@ -34,8 +32,6 @@ DOC.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrDOCmgL, color = catchment
   facet_wrap(~site, ncol = 3, scales = "free_y")
 
 #units for BBWM and LEF are incorrect...values too low
-
-ggsave(path = "plots", filename = "DOC.an.pdf", width = 11, height = 8)
 
 # NH4
 NH4.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrNH4NmgL, color = catchment, shape = type)) +
@@ -47,8 +43,6 @@ NH4.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrNH4NmgL, color = catchmen
 # SLP errant value in 2001?
 # LEF: elevated values 1980s...? TKH can check with McDowell
 
-ggsave(path = "plots", filename = "NH4.an.pdf", width = 11, height = 8)
-
 # NO3
 NO3.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrNO3NmgL, color = catchment, shape = type)) +
   geom_point() +
@@ -56,8 +50,6 @@ NO3.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrNO3NmgL, color = catchmen
   facet_wrap(~site, ncol = 3, scales = "free_y")
 
 # Should apply a common LOQ filter
-
-ggsave(path = "plots", filename = "NO3.an.pdf", width = 11, height = 8)
 
 # TDP
 TDP.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrTDPmgL, color = catchment, shape = type)) +
@@ -69,8 +61,6 @@ TDP.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrTDPmgL, color = catchment
 # Need to apply an LOQ filter
 # early record CWT & HBEF high values
 
-ggsave(path = "plots", filename = "TDP.an.pdf", width = 11, height = 8)
-
 # SO4
 SO4.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrSO4SmgL, color = catchment, shape = type)) +
   geom_point() +
@@ -79,8 +69,6 @@ SO4.an <- dat_ann %>% ggplot(aes(x = WaterYear, y = StrSO4SmgL, color = catchmen
 
 # Check units for DOR
 # Apply a common LOQ filter
-
-ggsave(path = "plots", filename = "SO4.an.pdf", width = 11, height = 8)
 
 ## Flow-weighted annual mean concentration ##
 # Calcium
@@ -118,3 +106,59 @@ SO4.fwa <- dat_ann %>% ggplot(aes(x = WaterYear, y = FWASO4SmgL, color = catchme
   geom_point() +
   geom_line() +
   facet_wrap(~site, ncol = 3, scales = "free_y")
+
+## Monthly mean concentration ##
+# Calcium
+Ca.fwm <- dat_mon %>% ggplot(aes(x = WaterYear, y = FWACamgL, color = catchment, shape = type)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~site, ncol = 3, scales = "free_y")
+
+# Slepers high outliers
+# Why does BBWM have annual FWACa, but not monthly?
+# Why does LEF have monthly FWACa, but not annual?
+# Units for LEF Ca are incorrect
+
+# DOC
+DOC.fwm <- dat_mon %>% ggplot(aes(x = WaterYear, y = FWADOCmgL, color = catchment, shape = type)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~site, ncol = 3, scales = "free_y")
+
+# Units for LEF DOC are incorrect
+# No monthly DOC for BBWM?
+# Check DOC outliers for HBEF
+
+#NH4
+NH4.fwm <- dat_mon %>% ggplot(aes(x = WaterYear, y = FWANH4NmgL, color = catchment, shape = type)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~site, ncol = 3, scales = "free_y")
+
+# Check NH4 units for LEF
+
+#NO3
+NO3.fwm <- dat_mon %>% ggplot(aes(x = WaterYear, y = FWANO3NmgL, color = catchment, shape = type)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~site, ncol = 3, scales = "free_y")
+
+# Units incorrect for LEF
+
+#TDP
+TDP.fwm <- dat_mon %>% ggplot(aes(x = WaterYear, y = FWATDPmgL, color = catchment, shape = type)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~site, ncol = 3, scales = "free_y")
+
+# Erroneous values at beginning of CWT record
+
+# SO4
+SO4.fwm <- dat_mon %>% ggplot(aes(x = WaterYear, y = FWASO4SmgL, color = catchment, shape = type)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~site, ncol = 3, scales = "free_y")
+
+ggsave(path = "plots", filename = "SO4.fwm.pdf", width = 11, height = 8)
+
+# Are the outlier values at SLP (~3 points) & TLW (~6 points) real?
