b52f2f7471df7148354169dc232baf9c4886315f
Tamara <tkharms@alaska.edu>
2020-09-20 22:39:42 -0800

    discharge munge script

--- data/04_discharge_wrangling.R
+++ data/04_discharge_wrangling.R
@@ -14,9 +14,9 @@ Qin <- import_list(here("data", "Daily Discharge - All Sites.xlsx"))
 # Replace problematic "-" with "_" in list element names (any of the following should work)
 Qin <- lapply(Qin, function(x) setNames(x, sub("-", "_", names(x)))) #this one worked once
 
-Qin <- map(Qin, ~sub("-", "_", names(.x)))
+#Qin <- map(Qin, ~sub("-", "_", names(.x)))
 
-Qin <- lapply(Qin, function(x) sub("-", "_", names(x)))
+#Qin <- lapply(Qin, function(x) sub("-", "_", names(x)))
 
 Qin <- lapply(Qin, function(x) {
   names(x)[grep("-", names(x))] <- "_"
@@ -29,6 +29,44 @@ change_names <- function(x) {
 Qin <- map(Qin, ~change_names(.x))
 
 ########
+## Going to brute force names of list items...ugh
+names(Qin)[1] <- "BBWM_EB"
+names(Qin)[2] <- "CWT_WS7"
+names(Qin)[3] <- "CWT_WS18"
+names(Qin)[4] <- "DOR_HP3"
+names(Qin)[5] <- "DOR_HP3A"
+names(Qin)[6] <- "DOR_HP5"
+names(Qin)[7] <- "DOR_HP6"
+names(Qin)[8] <- "DOR_HP6A"
+names(Qin)[9] <- "ELA_EIF"
+names(Qin)[10] <- "ELA_NEIF"
+names(Qin)[11] <- "ELA_NWIF"
+names(Qin)[12] <- "HBEF_WS1"
+names(Qin)[13] <- "HBEF_WS6"
+names(Qin)[14] <- "HBEF_WS7"
+names(Qin)[15] <- "HBEF_WS8"
+names(Qin)[16] <- "HBEF_WS9"
+names(Qin)[17] <- "HJA_WS6"
+names(Qin)[18] <- "HJA_WS7"
+names(Qin)[19] <- "HJA_WS8"
+names(Qin)[20] <- "HJA_WS9"
+names(Qin)[21] <- "HJA_WS10"
+names(Qin)[22] <- "LEF_QS"
+names(Qin)[23] <- "LEF_RI"
+names(Qin)[24] <- "LEF_MPR"
+names(Qin)[25] <- "MEF_S2"
+names(Qin)[26] <- "MEF_S4"
+names(Qin)[27] <- "MEF_S5"
+names(Qin)[28] <- "MEF_S6"
+names(Qin)[29] <- "SEF_WS77"
+names(Qin)[30] <- "SEF_WS80"
+names(Qin)[31] <- "SLP_WS9"
+names(Qin)[32] <- "TLW_C31"
+names(Qin)[33] <- "TLW_C32"
+names(Qin)[34] <- "TLW_C33"
+names(Qin)[35] <- "TLW_C34"
+names(Qin)[36] <- "TLW_C35"
+names(Qin)[37] <- "TLW_C38"
 
 # Add Site column to each list element
 Qin <- purrr::imap(Qin, ~mutate(.x, Site = .y))
@@ -44,6 +82,11 @@ Q_Ls <- mapply(cbind, Q_Ls, "discharge_units" = "L_s", SIMPLIFY = FALSE)
 
 # Column for Q in L s-1
 Q_Ls <- mapply(cbind, Q_Ls, "discharge_Ls" = NA, SIMPLIFY = FALSE)
+
+Q_Ls <- lapply(Q_Ls, function(x) {
+  x$discharge_Ls <- x[[2]]
+  return(x)
+})
 
 ## Sites in cfs = BBWM_EB, HJA_WS6, HJA_WS7, HJA_WS8, HJA_WS9, WJA_WS10, LEF_QS, LEF_RI
 cfs_sites <- c("BBWM_EB", "HJA_WS6", "HJA_WS7", "HJA_WS8", "HJA_WS9", "HJA_WS10", "LEF_QS", "LEF_RI")
@@ -164,3 +207,44 @@ MEF_S4$catch_area_km2 <- 0.340
 MEF_S5$catch_area_km2 <- 0.526
 MEF_S6$catch_area_km2 <- 0.089
 
+Q_cm <- list(MEF_S2 = MEF_S2,
+             MEF_S4 = MEF_S4,
+             MEF_S5 = MEF_S5,
+             MEF_S6 = MEF_S6)
+
+# Convert cm/d to L s-1: cm/d * catchment area (km2) * 10^5 (convert to L) * 1/86500 (convert d to s)
+Q_cm <- lapply(Q_cm, function(x) {
+  x$discharge_Ls = x[[2]]*x[['catch_area_km2']]*10^7*(1/86400)
+  return(x)
+})
+
+## Make a single data frame for output and plotting
+Qall <- list(Q_Ls, Q_cfs, Q_m3, Q_mm, Q_cm)
+Qall <- unlist(Qall, recursive = FALSE)
+
+Qall <- lapply(Qall, function(x) {
+  x$site <- x$Site
+  return(x)
+})
+
+Qall <- lapply(Qall, separate, site, c("studyarea", "watershed"))
+
+#Per Irena, drop LEF_MPR (also problems formatting date for this site)
+Qall <- within(Qall, rm(LEF_MPR)) 
+
+## Collapse to a single dataframe for plotting
+Qall.df <- bind_rows(Qall, .id = "SiteID")
+
+Qall.df <- Qall.df %>% select(SiteID:watershed, DATE)
+
+Qall.df$Date <- coalesce(Qall.df$Date, Qall.df$DATE)
+
+# Plot
+Qall.pl <- Qall.df %>% ggplot(aes(x = Date, y = discharge_Ls, color = watershed)) +
+  geom_point() +
+  geom_line() +
+  facet_wrap(~studyarea, scales = "free_y")
+
+ggsave(path = "plots", filename = "discharge.pdf", width = 10, height = 10, units = "in")
+
+write.csv(Qall.df, "Qall.df.csv", row.names = FALSE)
