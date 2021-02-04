9b943c6880e400baee97688ec67b20ea82dc274e
Jim Hood <hood.211@osu.edu>
2021-02-03 21:57:24 -0500

    updated ELA FWMC

--- analysis/01b_data_mungingTake2.R
+++ analysis/01b_data_mungingTake2.R
@@ -17,39 +17,51 @@ library(readxl)
 ## Load data
 ##-------------------
 
+# Common time period
+CTstart <- as.POSIXct(paste0("1985-10-31", format = "%Y-%m-%d"))
+CTend <- as.POSIXct(paste0("2010-10-31", format = "%Y-%m-%d"))
+# 9130 days
+
 # ELA
 # units on Q are m3/s
+# units for solutes variable in ELA stream chemistry.xls::Units
 # no info on qualifiers
 
 # ELA EIF
-# 318 NAs/ 16802, 16,802 days 
-#1971-2016
 ELA.eif <- readxl::read_xlsx(file.path(here::here("data/NewDataFromIrena20210130/New MAR Data/Raw Data Files/ELA"), 
                                           "ELA EIF discharge.xlsx"),1) %>% 
-        mutate_at(vars(data_set:station,qualifier), factor)
+        mutate_at(vars(data_set:station,qualifier), factor) %>% 
+  mutate(mean_date = as.POSIXct(mean_date, format = "%Y-%m-%d")) %>% 
+  # for some reason I can only get the start date if I have >= the day before i start
+  filter(mean_date >= CTstart & mean_date <= CTend) %>% 
+  mutate(dateC = as.character(as.Date(mean_date))) %>% 
+  rename("Q_m3s" = "mean_daily_discharge")
 
-ggplot(ELA.eif, aes(y = mean_daily_discharge, mean_date)) +
+ggplot(ELA.eif, aes(y = Q_m3s, mean_date)) +
   geom_point()
 
 # ELA NEIF
-# 104 NAs/16606
-# 1971 to 2016 - 16,748 days
-# There are 142 days missing here
 ELA.neif <- readxl::read_xlsx(file.path(here::here("data/NewDataFromIrena20210130/New MAR Data/Raw Data Files/ELA"), 
                                        "ELA NEIF discharge.xlsx"),1) %>% 
-  mutate_at(vars(data_set:station,qualifier), factor)
+  mutate_at(vars(data_set:station,qualifier), factor) %>% 
+  mutate(mean_date = as.POSIXct(mean_date, format = "%Y-%m-%d")) %>% 
+  filter(mean_date >= CTstart & mean_date <= CTend) %>% 
+  mutate(dateC = as.character(as.Date(mean_date)))%>% 
+  rename("Q_m3s" = "mean_daily_discharge")
 
-ggplot(ELA.neif, aes(y = mean_daily_discharge, mean_date)) +
+ggplot(ELA.neif, aes(y = Q_m3s, mean_date)) +
   geom_point()
 
 # ELA NWIF
-# 227 NAs/127113 
-# 1970 to 2016 - 17,113 days
 ELA.nwif <- readxl::read_xlsx(file.path(here::here("data/NewDataFromIrena20210130/New MAR Data/Raw Data Files/ELA"), 
                                         "ELA NWIF discharge.xlsx"),1) %>% 
-  mutate_at(vars(data_set:station,qualifier), factor)
+  mutate_at(vars(data_set:station,qualifier), factor) %>% 
+  mutate(mean_date = as.POSIXct(mean_date, format = "%Y-%m-%d")) %>% 
+  filter(mean_date >= CTstart & mean_date <= CTend)%>% 
+  mutate(dateC = as.character(as.Date(mean_date)))%>% 
+  rename("Q_m3s" = "mean_daily_discharge")
 
-ggplot(ELA.neif, aes(y = mean_daily_discharge, mean_date)) +
+ggplot(ELA.nwif, aes(y = Q_m3s, mean_date)) +
   geom_point()
 
 # ELA chemistry
@@ -70,25 +82,100 @@ BadCodeNAfun <- function(x){
 
 
 ELA.chem <- readxl::read_xlsx(file.path(here::here("data/NewDataFromIrena20210130/New MAR Data/Raw Data Files/ELA"), 
-                                        "ELA stream chemistry.xlsx"),2) %>% 
+                                        "ELA stream chemistry.xlsx"),2)%>% 
+  mutate(SAMPLED = as.POSIXct(SAMPLED, format = "%Y-%m-%d")) %>% 
+  filter(SAMPLED >= CTstart & SAMPLED <= CTend) %>%
   mutate_at(vars(TYPE:STATION), factor) %>% 
   # select solutes of interest
-  select("SAMPLED", "SUBLOC", "STATION", "CA", "DOC", "NH4", "NO3", "TDP", "SO4") %>% 
+  #NH4 was labeled as NH3 in excel sheet:units
+  select("SAMPLED", "SUBLOC", "STATION","CA","DOC","NH4","NO3","TDP","SO4") %>% 
+  rename(CA_mgL = "CA", DOC_uM = "DOC", NH4_ugL = "NH4",NO3_ugL ="NO3",TDP_ugL = "TDP",SO4_mgL = "SO4") %>% 
   # select focal catchments
   filter(STATION %in% c("EIF", "NWIF", "NEIF")) %>% 
   # remove solute values that are codes (all below zero)
-  mutate(across(CA:SO4, BadCodeNAfun))#ifelse(. <= 0, as.numeric("NA"),.)
-
+  mutate(across(CA_mgL:SO4_mgL, BadCodeNAfun)) %>% #ifelse(. <= 0, as.numeric("NA"),.)
+  mutate(dateC = as.character(as.Date(SAMPLED))) %>% 
+  # more than one sample on some days - average them
+  group_by(dateC, STATION) %>% 
+  summarize(across(c(CA_mgL:SO4_mgL), mean, na.rm = TRUE)) %>% 
+  mutate(mean_date = as.POSIXct(dateC, format = "%Y-%m-%d"))
 
 ggplot(ELA.chem %>% 
-         pivot_longer(CA:SO4, names_to = "solutes", values_to = "values"), aes(y = values, x = SAMPLED)) +
+         pivot_longer(CA_mgL:SO4_mgL, names_to = "solutes", values_to = "values"), aes(y = values, x = mean_date, color = STATION)) +
   geom_point() +
   facet_wrap(vars(solutes), scales = "free_y")
 
+ELA.chemL <- ELA.chem %>% 
+  pivot_longer(cols = CA_mgL:SO4_mgL, names_to = "solute", values_to = "conc") %>% 
+  mutate(solute = as.factor(solute))
+
 # COMBINE Q AND CHEM
 ELA.eif2 <- ELA.eif %>% 
-  full_join(ELA.chem %>% 
-              filter(STATION == "EIF"), by = c("mean_date" = "SAMPLED"))
+  left_join(ELA.chemL %>% 
+              filter(STATION == "EIF"), by = "dateC") %>% 
+  select(dateC, date = "mean_date.x", Q_m3s, solute, conc) %>% 
+  mutate(station = "EIF",
+         Ym = as.character(strftime(date, format = "%Y-%m")), 
+         # using eq here: https://ncwqr.files.wordpress.com/2017/06/d-time-weighted-and-flow-weighted-mean-concentrations.pdf
+        FWMCtop = conc * (Q_m3s*60*60*24) * 1, #[conc * (flow converted to d) * 1day]
+         FWMCbottom = (Q_m3s*60*60*24) * 1,
+         # don't want to divide "top" by a larger number of days of bottom
+         FWMCbottomUse = ifelse(!is.na(FWMCtop), FWMCbottom, as.numeric("NA"))) %>% # [(flow conv to d) * 1 day]
+  group_by(Ym, solute, station) %>% 
+  summarize(across(c(FWMCtop, FWMCbottomUse), mean, na.rm = T)) %>% 
+  mutate(FWMC = FWMCtop/FWMCbottomUse) %>% 
+  mutate(date = as.POSIXct(paste0(Ym,"-01"), format = "%Y-%m-%d")) %>% 
+  #remove 301 NA's in solute
+  filter(solute != "NA")
+
+ggplot(ELA.eif2, aes(y = FWMC, x = date, color = solute)) +
+  geom_point() +
+  facet_wrap(vars(solute), scales = "free_y")
+
+
+ELA.neif2 <- ELA.neif %>% 
+  full_join(ELA.chemL %>% 
+              filter(STATION == "NEIF"), by = "dateC")%>% 
+  select(dateC, date = "mean_date.x", Q_m3s, solute, conc) %>% 
+  mutate(station = "NEIF",
+         Ym = as.character(strftime(date, format = "%Y-%m")), 
+         # using eq here: https://ncwqr.files.wordpress.com/2017/06/d-time-weighted-and-flow-weighted-mean-concentrations.pdf
+         FWMCtop = conc * (Q_m3s*60*60*24) * 1, #[conc * (flow converted to d) * 1day]
+         FWMCbottom = (Q_m3s*60*60*24) * 1,
+         # don't want to divide "top" by a larger number of days of bottom
+         FWMCbottomUse = ifelse(!is.na(FWMCtop), FWMCbottom, as.numeric("NA"))) %>% # [(flow conv to d) * 1 day]
+  group_by(Ym, solute, station) %>% 
+  summarize(across(c(FWMCtop, FWMCbottomUse), mean, na.rm = T)) %>% 
+  mutate(FWMC = FWMCtop/FWMCbottomUse) %>% 
+  mutate(date = as.POSIXct(paste0(Ym,"-01"), format = "%Y-%m-%d")) %>% 
+  #remove NA's in solute
+  filter(solute != "NA")
+
+ggplot(ELA.neif2, aes(y = FWMC, x = date, color = solute)) +
+  geom_point() +
+  facet_wrap(vars(solute), scales = "free_y")
+
+ELA.nwif2 <- ELA.nwif %>% 
+  full_join(ELA.chemL %>% 
+              filter(STATION == "NWIF"), by = "dateC")%>% 
+  select(dateC, date = "mean_date.x", Q_m3s, solute, conc) %>% 
+  mutate(station = "NWIF",
+         Ym = as.character(strftime(date, format = "%Y-%m")), 
+         # using eq here: https://ncwqr.files.wordpress.com/2017/06/d-time-weighted-and-flow-weighted-mean-concentrations.pdf
+         FWMCtop = conc * (Q_m3s*60*60*24) * 1, #[conc * (flow converted to d) * 1day]
+         FWMCbottom = (Q_m3s*60*60*24) * 1,
+         # don't want to divide "top" by a larger number of days of bottom
+         FWMCbottomUse = ifelse(!is.na(FWMCtop), FWMCbottom, as.numeric("NA"))) %>% # [(flow conv to d) * 1 day]
+  group_by(Ym, solute, station) %>% 
+  summarize(across(c(FWMCtop, FWMCbottomUse), mean, na.rm = T)) %>% 
+  mutate(FWMC = FWMCtop/FWMCbottomUse) %>% 
+  mutate(date = as.POSIXct(paste0(Ym,"-01"), format = "%Y-%m-%d")) %>% 
+  #remove NA's in solute
+  filter(solute != "NA")
+
+ggplot(ELA.nwif2, aes(y = FWMC, x = date, color = solute)) +
+  geom_point() +
+  facet_wrap(vars(solute), scales = "free_y")
 
 ########## NEED TO CHECK THIS #########
 
