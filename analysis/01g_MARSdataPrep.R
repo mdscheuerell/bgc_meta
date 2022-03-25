e8aa89ac2980528fc7672adf9da4d2712a127ff9
Jim Hood <hood.211@osu.edu>
2022-03-25 11:39:58 -0400

    one more cleanup

--- analysis/01g_MARSdataPrep.R
+++ analysis/01g_MARSdataPrep.R
@@ -80,3 +80,4 @@ MARSmissingDataByWS <- MARSdf2 %>%
 # write.csv(MARSmissingDataByWS, file.path(here::here("data/JMHnewMungedDat"), "01g_NumberOfNAsInMARSdf.csv"))
 # write.csv(MARSmissingDataByWS, file.path(here::here("data/JMHnewMungedDat"), "01g_Dat4MARS_FWMCmgElementL.csv"))
 # save.image(file.path(here::here("analysis"), "01gg_MARSdataPrep_Rdat"))
+load(file.path(here::here("analysis"), "01gg_MARSdataPrep_Rdat"))
